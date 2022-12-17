af8990f5932557dfe703a2dc7d4d787fdd50234f
Damacy64 <pzx6464@gmail.com>
2022-12-16 20:25:34 -0600

    Subiendo archivos

---
+++ juego/ProyectoJuego.java
@@ -0,0 +1,50 @@
+package juego;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author pzx64
+ */
+public class ProyectoJuego {
+
+    int x;
+    int y;
+    JPanel miJP;
+    String Dir;
+    int Ancho;
+    int Alto;
+
+    ProyectoJuego(JPanel JP) {
+        this.x = 0;
+        this.y = 0;
+        this.miJP = JP;
+        this.Ancho = 0;
+        this.Alto = 0;
+    }
+
+    public void Dibuja_feid(Graphics g, int xi, int yi, String Direccion) {
+        this.x = xi;
+        this.y = yi;
+        this.miJP.update(g);
+        this.Dir = Direccion;
+        ImageIcon MImagen = new ImageIcon(Direccion);
+        g.drawImage(MImagen.getImage(), xi, yi, miJP);
+        Alto = MImagen.getIconHeight();
+        Ancho = MImagen.getIconWidth();
+    }
+
+    public int CordX() {
+        return this.x;
+    }
+
+    public int CordY() {
+        return this.y;
+    }
+
+    public String Direction() {
+        return this.Dir;
+    }
+}
---
+++ juego/Salto.java
@@ -0,0 +1,54 @@
+package juego;
+
+import java.awt.Graphics;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author pzx64
+ */
+public class Salto extends Thread {
+
+    int x, y;
+    ProyectoJuego Feid;
+    JPanel miJP;
+    Graphics g;
+    String direc;
+
+    public Salto(JPanel miJ, int xi, int yi, ProyectoJuego FJ) {
+        super();
+        this.g = miJ.getGraphics();
+        this.miJP = miJ;
+        this.x = xi;
+        this.y = yi;
+        this.Feid = FJ;
+        this.direc = FJ.Direction();
+
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        int z;
+        z = this.y;
+        while (true) {
+            while (i <= 8) {
+
+                try {
+
+                    this.y = z + 3 * i * i - 24 * i;
+                    this.miJP.update(g);
+                    Feid.Dibuja_feid(g, this.x, this.y, this.direc);
+
+                    Salto.sleep(50);
+                    i = i + 1;
+
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Salto.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+}
